30e69da2537ab1b29988a89bab4c61d8d2478cb2
mrozinskipatryk <patryk.mrozinski@monterail.com>
2024-05-01 22:12:27 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `boat-log`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-theme-ui"]
+};
